712a7172b353edaa971375dcf6be1e64e6728b26
nthavamani <nthavamani@IN-PG02A8E3>
2022-05-24 12:02:35 +0530

    Add project files.

---
+++ Helloworld_TestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTestingusing System.IO;
+using System;
+
+
+namespace Helloworld_TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
