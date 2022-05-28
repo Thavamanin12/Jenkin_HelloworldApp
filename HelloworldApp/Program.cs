6cfda1c7c7cf8cb191720f87f42015ddd239c0cf
N Thavamani <n.thavamani@nagarro.com>
2022-05-28 12:24:09 +0000

    Updated Program.cs for cherry -pick demo

--- HelloworldApp/Program.cs
+++ HelloworldApp/Program.cs
@@ -7,6 +7,8 @@ namespace HelloworldApp
        public  static void Main()
         {
             Console.WriteLine("Hello World!");
+            console.WriteLine("cheery-pick demo");
+        }
         }
     }
 }
